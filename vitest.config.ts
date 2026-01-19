41b3c33fdd414e2babc02e255dac07dd42968af2
Jou Okuyama <ojoxux@gmail.com>
2026-01-19 10:12:13 +0900

    test: add Vitest configuration for testing with Workers

---
+++ vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineWorkersProject } from '@cloudflare/vitest-pool-workers/config'
+
+export default defineWorkersProject({
+  test: {
+    globals: true,
+    include: ['app/**/*.test.ts'],
+    setupFiles: ['./app/test/setup.ts'],
+    poolOptions: {
+      workers: {
+        wrangler: { configPath: './wrangler.jsonc' },
+        miniflare: {
+          d1Databases: ['DB'],
+          kvNamespaces: ['KV'],
+        },
+      },
+    },
+  },
+})
